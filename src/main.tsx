2c72910bc3e92f994b9c7dc2e079928b82c8ca03
rami242424 <mickygamja@naver.com>
2025-08-07 10:00:00 +0900

    feat: add routing skeleton

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/catalog" element={<div>Catalog Page</div>} />
+        <Route path="/cart" element={<div>Cart Page</div>} />
+        <Route path="/checkout" element={<div>Checkout Page</div>} />
+        <Route path="/admin" element={<div>Admin Page</div>} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
