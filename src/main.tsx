48cc071282ba754372accbedbb1f6e1e65aa289b
rami242424 <mickygamja@naver.com>
2025-09-15 13:01:22 +0900

    main.tsx edit - route delete

--- src/main.tsx
+++ src/main.tsx
@@ -1,29 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Toaster } from 'sonner'
-import App from './App.tsx'
-import CatalogPage from './features/catalog/CatalogPage.tsx'
-import CartPage from './features/order/CartPage.tsx'
-import CheckoutPage from './features/order/CheckoutPage.tsx'
-import CheckoutSuccessPage from './features/order/ChecoutSuccessPage.tsx'
-import Navbar from './components/Navbar.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <Navbar />
-      {/* 토스트 컨테이너 */}
-      <Toaster richColors position="top-center" />
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-        <Route path="/checkout/success" element={<CheckoutSuccessPage />} />
-        <Route path="/admin" element={<div className="mx-auto max-w-[var(--container)] px-4 py-6">Admin Page</div>} />
-      </Routes>
+      <App />   
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
